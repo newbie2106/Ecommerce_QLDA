72cadeb70734708f34c2ce17063ebdaa74682932
Tuan Nhat <tuannhat226@gmail.com>
2024-09-18 22:29:22 +0700

    delete all

--- SaleWebsiteQLDA_FE/SaleWebsiteQLDA_FE/src/reducers/MyCartReducer.js
+++
@@ -1,8 +0,0 @@
-const MyCartReducer = (currentState, action) => {
-    if (action.type === 'update')
-        return currentState + action.payload;
-
-    return currentState;
-}
-
-export default MyCartReducer;
